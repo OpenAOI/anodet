7e72540413cf72537bf9dd67782c29441fefd365
Anton Eriksson <antoeri@student.chalmers.se>
2021-08-06 17:28:32 +0200

    Added boundary and heatmap

--- anodet/visualization.py
+++ anodet/visualization.py
@@ -5,63 +5,92 @@ Provides functions for visualizing results of anomaly detection.
 import cv2
 import numpy as np
 from skimage.segmentation import mark_boundaries
+from typing import Union, Tuple, Optional, cast
+import torch
 
 
+def boundary_image(image: Union[np.ndarray, torch.Tensor],
+                   mask: Union[np.ndarray, torch.Tensor],
+                   color: Optional[Tuple[int, int, int]]=(255, 0, 0)
+                  ):
+    """
+    Draw boundaries around masked areas on image.
 
-def boundary_image_classification_group(images, patch_classifications,
-                                        image_classifications, size):
+    Args:
+        image: Image on which to draw boundaries.
+        mask: Mask defining the areas.
+        color: Color of boundaries.
 
-    padding = 30
-    margin = 50
-    tot_image = np.ones((size+2*padding, len(images)*(size+2*padding)\
-                         +(len(images)-1)*margin, 3)).astype(np.uint8)*255
+    Returns:
+        boundary_image: Image with boundaries.
 
-    for i, image in enumerate(images):
-        b_image = boundary_image_classification(image, patch_classifications[i],
-                                                image_classifications[i],
-                                                size, padding=padding)
-        height = b_image.shape[0]
-        tot_image[:, i*(height+margin):(i+1)*(height)+i*margin, :] = b_image
+    """
 
-    return tot_image
+    if type(image) == torch.Tensor:
+        image = image.cpu().numpy()
+    if type(mask) == torch.Tensor:
+        mask = mask.cpu().numpy()
 
+    image = cast(np.ndarray, image)
+    mask = cast(np.ndarray, mask)
 
+    boundary_image = image.copy()
+    height, width, channels = boundary_image.shape
 
-def boundary_image_classification(image, patch_classification,
-                                  image_classification, size, padding=20):
+    # Draw boundaries
+    boundaries = np.zeros(mask.shape)
+    boundaries = mark_boundaries(boundaries, mask, color=(1, 0, 0), mode='thick')
+    boundaries = cv2.resize(boundaries, (width, height), interpolation=cv2.INTER_AREA)
+    boundaries = boundaries[:, :, 0].astype(bool)
 
-    frame = (np.ones((size+2*padding, size+2*padding, 3))*255).astype(np.uint8)
+    # Draw boundaries on image
+    boundary_image[boundaries] = color
 
-    if image_classification:
-        frame[:, :, 0] = 0
-        frame[:, :, 2] = 0
-    else:
-        frame[:, :, 1] = 0
-        frame[:, :, 2] = 0
-
-    b_image = boundary_image(image, patch_classification, size)
-    frame[padding:frame.shape[0]-padding, padding:frame.shape[1]-padding] = b_image
-    return frame
+    return boundary_image
 
 
+def heatmap_image(image: Union[np.ndarray, torch.Tensor],
+                  score_map: Union[np.ndarray, torch.Tensor],
+                  interval: Tuple[float, float]
+                 ):
+    """
+    Draw heatmap on image based on score_map.
 
-def boundary_image(image, patch_classification, size):
+    Args:
+        image: Image on which to the heatmap.
+        score_map: Array which the heatmap is generated from.
+        interval: Range of the heatmap (min_value, max_value)
 
-    image = image.copy()
-    image = cv2.resize(image, (size, size), interpolation=cv2.INTER_AREA)
+    Returns:
+        heatmap_image: Image with applied heatmap.
 
-    mask = patch_classification.cpu().numpy()
+    """
 
-    transparent = np.zeros(mask.shape)
-    line_img = mark_boundaries(transparent, mask, color=(1, 0, 0), mode='thick')
-    line_img = cv2.resize(line_img, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_AREA)
-    line_img = (line_img*255).astype(np.uint8)
+    if type(image) == torch.Tensor:
+        image = image.cpu().numpy()
+    if type(score_map) == torch.Tensor:
+        score_map = score_map.cpu().numpy()
 
-    line_mask = line_img == [255, 0, 0]
-    line_mask = line_mask[:, :, 0]
+    image = cast(np.ndarray, image)
+    score_map = cast(np.ndarray, score_map)
 
-    image[:, :, 0] = np.where(line_mask, line_img[:, :, 0], image[:, :, 0])
-    image[:, :, 1] = np.where(line_mask, 0, image[:, :, 1])
-    image[:, :, 2] = np.where(line_mask, 0, image[:, :, 1])
+    heatmap_image = image.copy()
+    score_map = score_map.copy()
+    height, width, channels = heatmap_image.shape
+    min_value = interval[0]
+    max_value = interval[1]
 
-    return image
+    # Clip
+    score_map[score_map<min_value] = min_value
+    score_map[score_map>=max_value] = max_value
+
+    # Prepare heatmap
+    score_map_norm = ((score_map - min_value) / max_value)
+    score_map_norm_inv = ((1 - score_map_norm) * 255).astype(np.uint8)
+    heatmap = cv2.applyColorMap(score_map_norm_inv, colormap=cv2.COLORMAP_JET)
+    heatmap = cv2.resize(heatmap, (width, height), interpolation=cv2.INTER_AREA)
+
+    # Blend images
+    heatmap_image = cv2.addWeighted(heatmap, 0.5, heatmap_image, 0.5, 0)
+
+    return heatmap_image
