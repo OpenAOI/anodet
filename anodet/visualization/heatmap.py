8c9ee1c49bb44c9f1e100218ee74cee0362d0db0
Anton Maxén <Anton.maxen@gmail.com>
2021-09-06 10:52:31 +0200

    Fixes and improvement of heatmap functions. (#12)
    * modified heatmap to accept mask
    
    * updated heatmap with mask working
    
    * fixed docstrings properly

--- anodet/visualization/heatmap.py
+++ anodet/visualization/heatmap.py
@@ -8,6 +8,7 @@ from typing import Union, Optional
 
 def heatmap_images(images: Union[np.ndarray, torch.Tensor],
                    list_of_patch_scores: Union[np.ndarray, torch.Tensor],
+                   masks: Optional[Union[np.ndarray, torch.Tensor]] = None,
                    min_v: Optional[float] = None,
                    max_v: Optional[float] = None,
                    alpha: float = 0.6) -> np.ndarray:
@@ -17,6 +18,7 @@ def heatmap_images(images: Union[np.ndarray, torch.Tensor],
     Args:
         images: The images to draw heatmaps on.
         list_of_patch_scores: The values to use to generate colormap.
+        masks: array of masks on where to draw heatmap on images.
         min_v: min value for normalization
         max_v: max value for normalization
         alpha: The opacity of the colormap
@@ -25,17 +27,22 @@ def heatmap_images(images: Union[np.ndarray, torch.Tensor],
         heatmaps: a array of heatmaps.
 
     """
+    heatmaps = []
     images = to_numpy(images).copy()
     list_of_patch_scores = to_numpy(list_of_patch_scores).copy()
-    heatmaps = []
+
+    if isinstance(masks, (np.ndarray, torch.Tensor)):
+        masks = to_numpy(masks).copy()
 
     norm_patch_scores = normalize_patch_scores(
         list_of_patch_scores,
         min_v=min_v,
         max_v=max_v
     )
+
     for i, score in enumerate(norm_patch_scores):
-        image_heatmap = heatmap_image(images[i], score, alpha=alpha)
+        mask = masks[i] if isinstance(masks, (np.ndarray, torch.Tensor)) else None
+        image_heatmap = heatmap_image(images[i], score, mask=mask, alpha=alpha)
         heatmaps.append(image_heatmap)
 
     return np.array(heatmaps)
@@ -43,6 +50,7 @@ def heatmap_images(images: Union[np.ndarray, torch.Tensor],
 
 def heatmap_image(image: Union[np.ndarray, torch.Tensor],
                   patch_scores: Union[np.ndarray, torch.Tensor],
+                  mask: Optional[Union[np.ndarray, torch.Tensor]] = None,
                   min_v: Optional[float] = None,
                   max_v: Optional[float] = None,
                   alpha: float = 0.6) -> np.ndarray:
@@ -53,6 +61,7 @@ def heatmap_image(image: Union[np.ndarray, torch.Tensor],
     Args:
         image: image to draw the colormap on.
         patch_scores: patch scores or normalized ones
+        mask: mask where to draw heatmap on image.
         min_v: min value for normalization
         max_v: max value for normalization
         alpha: Opacity on the colormap
@@ -65,6 +74,10 @@ def heatmap_image(image: Union[np.ndarray, torch.Tensor],
     image = to_numpy(image).copy()
     patch_scores = to_numpy(patch_scores).copy()
 
+    if isinstance(mask, (np.ndarray, torch.Tensor)):
+        mask = to_numpy(mask).copy()
+        mask = np.logical_not(mask).astype(np.uint8)
+
     if min_v and max_v:
         patch_scores = normalize_patch_scores(
             patch_scores,
@@ -75,6 +88,6 @@ def heatmap_image(image: Union[np.ndarray, torch.Tensor],
     patch_scores = (1 - patch_scores) * 255
     patch_scores = patch_scores.astype(np.uint8)
     color_map = cv2.applyColorMap(patch_scores, colormap=cv2.COLORMAP_JET)
-    heatmap = blend_image(image, color_map, alpha=alpha)
+    heatmap = blend_image(image, color_map, alpha=alpha, mask=mask)
 
     return heatmap
