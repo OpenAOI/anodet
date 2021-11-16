2e875ca2d45c904fb846dea67142ebcfe7ffb441
victorolof <olofssonvictor@gmail.com>
2021-11-16 11:37:40 +0100

    Padim script

--- notebooks/padim_script.py
+++ notebooks/padim_script.py
@@ -3,16 +3,35 @@ Padim Script
 """
 
 import os
-import anodet
-import numpy as np
-import torch
+import time
+from time import gmtime
+from time import strftime
+
 import cv2
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import numpy as np
+import openpyxl
+import torch
+from sklearn.metrics import roc_auc_score
+from torch.utils.data import DataLoader
 from torchvision import transforms as T
 
+import anodet
 
-def visual(test_images, score_map_classifications, image_classifications, score_maps, images, image_scores):
+
+def visual_image_result(test_images, score_map_classifications, image_classifications, score_maps, images, image_scores):
+    """
+    Visual image result with heatmaps
+
+        Args:
+            test_images: Images on which to draw boundaries
+            score_map_classifications: Anomaly classifications about the images
+            image_classifications: Information about, if the images have anomalies
+            score_maps: The values to use to generate colormap
+            images: The images to be drawn upon
+            image_scores: Image level scores
+
+    """
     boundary_images = anodet.visualization.framed_boundary_images(test_images, score_map_classifications,
                                                                   image_classifications, padding=40)
     heatmap_images = anodet.visualization.heatmap_images(test_images, score_maps, alpha=0.5)
@@ -28,39 +47,94 @@ def visual(test_images, score_map_classifications, image_classifications, score_
         plt.show()
 
 
-def run(dataset_path: str = '../../data/pscb/good_cropped_masked',
-        test_images_path: str = '../../data/pscb/test_good_cropped_masked',
-        model_data_path: str = './distributions/',
-        backbone: str = 'resnet18',
-        layer_indices=None,
-        tresh: int = 13,
-        extractions: int = 1,
-        test_images_limit=10,
-        image_transforms: T.Compose = T.Compose([T.Resize(224),
-                                                 T.CenterCrop(224),
-                                                 T.ToTensor(),
-                                                 T.Normalize(mean=[0.485, 0.456, 0.406],
-                                                             std=[0.229, 0.224, 0.225])
-                                                 ])
-        ):
+def write_dict_values_to_sheet(file_path, dict_parameters):
+    """
+    Write dictionary values to excel sheet
+
+    Args:
+        file_path: Path to sheet in .xlsx format
+        dict_parameters: Dictionary with k/v pairs.
+
+    """
+    # Open sheet
+    book = openpyxl.load_workbook(file_path)
+    sheet = book.active
+
+    # Write to latest row
+    row = sheet.max_row + 1
+    for i, val in enumerate(dict_parameters.values()):
+        c1 = sheet.cell(row=row, column=i + 2)
+        c1.value = str(val)
+    book.save(file_path)
+
+
+def run_padim_script(dataset_path=os.path.realpath('../../data/pscb/good_cropped_masked'),
+                     test_good_images_path=os.path.realpath('../../data/pscb/test_good_cropped_masked'),
+                     test_bad_images_path=os.path.realpath('../../data/pscb/bad_cropped_masked'),
+                     backbone: str = 'resnet34',
+                     tresh: int = 11,
+                     extractions: int = 1,
+                     train_images_limit=None,
+                     test_good_images_limit=None,
+                     test_bad_images_limit=None,
+                     result_sheet_path=None,
+                     visualize_result=True,
+                     image_transforms: T.Compose = T.Compose([T.Resize(112),
+                                                              T.CenterCrop(112),
+                                                              T.ToTensor(),
+                                                              T.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                          std=[0.229, 0.224, 0.225])
+                                                              ])
+                     ):
+    """
+    Script to run Padim model using AnodetDataset.
+
+        Args:
+            dataset_path: Path for training images.
+            test_good_images_path: Path to good test images.
+            test_bad_images_path: Path to bad test images.
+            backbone: The name of the desired backbone. Must be one of: [resnet18, wide_resnet50]
+            tresh: A treshold value. If an image score is larger than
+                or equal to thresh it is classified as anomalous.
+            extractions: Number of extractions from dataloader. Could be of interest \
+                when applying random augmentations.
+            train_images_limit: Limit number of images to train on model.
+            test_good_images_limit: Limit number of good images to test on model.
+            test_bad_images_limit: Limit number of good bad to test on model.
+            result_sheet_path: Path to excel sheet with .xlsx extension, leave empty to skip.
+            visualize_result: View test images. Visualize results of binary prediction.
+            image_transforms: Torchvision Compose obj containing transforms
+
+        Returns:
+            image_classification_target: Numpy array with target classification
+            image_classifications: Numpy array with predicted image classifcations
+
+    """
+    # Start time
+    start_time = time.time()
+
     # Load dataset
     dataset = anodet.AnodetDataset(image_directory_path=dataset_path,
-                                   image_transforms=image_transforms)
-    dataloader = DataLoader(dataset, batch_size=32)
+                                   image_transforms=image_transforms,
+                                   images_limit=train_images_limit)
+    dataloader = DataLoader(dataset, batch_size=8)
 
     # Init the model
-    padim = anodet.Padim(backbone=backbone)
-    padim.fit(dataloader=dataloader, extractions=extractions)
+    model = anodet.Padim(backbone=backbone)
+    model.fit(dataloader=dataloader, extractions=extractions)
 
-    # Save the necessary parameters
-    distributions_path = './distributions/'
-    torch.save(padim.mean, os.path.join(model_data_path, "pscb_mean.pt"))
-    torch.save(padim.cov_inv, os.path.join(model_data_path, "pscb_inv.pt"))
+    # Load good and bad test images
+    good_image_paths = anodet.get_paths_for_directory_path(
+        directory_path=test_good_images_path,
+        limit=test_good_images_limit)
+    bad_image_paths = anodet.get_paths_for_directory_path(
+        directory_path=test_bad_images_path,
+        limit=test_bad_images_limit)
+    test_paths = good_image_paths + bad_image_paths
 
-    # Load test images
-    test_paths = anodet.get_paths_for_directory_path(
-        directory_path=test_images_path,
-        limit=test_images_limit)
+    # Target array image level
+    image_classification_target = [1] * len(good_image_paths) + [0] * len(bad_image_paths)
+    image_classification_target = np.array(image_classification_target)
 
     images = []
     for path in test_paths:
@@ -70,25 +144,33 @@ def run(dataset_path: str = '../../data/pscb/good_cropped_masked',
 
     batch = anodet.to_batch(images, image_transforms, torch.device('cpu'))
 
-    # Load the model data
-    mean = torch.load(os.path.join(model_data_path, 'pscb_mean.pt'))
-    cov_inv = torch.load(os.path.join(model_data_path, 'pscb_inv.pt'))
-
-    # Init the model
-    padim = anodet.Padim(backbone=backbone, mean=mean, cov_inv=cov_inv, device=torch.device('cpu'))
-
     # Make prediction
-    image_scores, score_maps = padim.predict(batch)
+    image_scores, score_maps = model.predict(batch)
 
-    # Interpret the prediction
+    # Interpret the prediction TODO: Classifications on patch level
     score_map_classifications = anodet.classification(score_maps, tresh)
     image_classifications = anodet.classification(image_scores, tresh)
+    image_classifications = image_classifications.numpy()
 
-    # Visualization
-    test_images = np.array(images).copy()
+    # Calculate time
+    end_time = time.time() - start_time
+    end_time = strftime("%H:%M:%S", gmtime(end_time))
 
-    # Run visualization... TODO: Save to file
-    visual(test_images, score_map_classifications, image_classifications, score_maps, images, image_scores)
+    # Visualize result
+    if visualize_result:
+        test_images = np.array(images).copy()
+        visual_image_result(test_images, score_map_classifications, image_classifications, score_maps, images, image_scores)
+        anodet.visualize_eval_pair(image_classification_target, image_classifications)
 
+    # Save result to sheet
+    if result_sheet_path:
+        score_roc_auc = roc_auc_score(image_classification_target,
+                                      image_classifications)
+        precision, recall, thresholds = anodet.optimal_threshold(image_classification_target,
+                                                                 image_classifications)
+        sheet_info = {"score_roc_auc": score_roc_auc, "optimal_trest": thresholds, "precision": precision, "recall":
+                      recall, "time": end_time, "backbone": backbone, "tresh": tresh, "extractions": extractions,
+                      "train_images_limit": train_images_limit, "image_transforms": str(image_transforms)}
+        write_dict_values_to_sheet(result_sheet_path, sheet_info)
 
-run()
+    return image_classification_target, image_classifications
